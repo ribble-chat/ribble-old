3544f6c16e5f00eecb363d89b6fcdf053cd26119
Jennifer Ong <jennifer.ex.ong@gmail.com>
2021-02-01 13:03:14 +1300

    remove git merge marker

--- src/components/chat/Chat.svelte
+++ src/components/chat/Chat.svelte
@@ -14,19 +14,11 @@
 
 <body>
   <div id="chat-container">
-<<<<<<< HEAD
-    <button on:click={() => api.callServer("user", "hiretard")}
-      >click me lul</button
-    >
+    <button on:click={() => api.callServer("user", "hiretard")}>
+      click me lul
+    </button>
     <MenuBar {person} on:click={() => console.log("ur mum")} />
     <ChatBox {me} />
-=======
-    <button on:click="{() => api.callServer('user', 'hiretard')}"
-      >click me lul</button
-    >
-    <MenuBar person="{person}" on:click="{() => console.log('ur mum')}" />
-    <ChatBox me="{me}" />
->>>>>>> 132832bcfb96307978d647cba01857e66ee6f915
     <ChatBar />
   </div>
 </body>
